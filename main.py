4b2867525cbea4b1e9572d204c06d0d7d8046626
arogeau <antoine.rogeau@mines-paristech.fr>
2022-05-06 18:05:18 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from math2 import add_numbers
+from person import Person
+
+
+def alex_method(name):
+    p = Person(name)
+    print(p)
+
+
+if __name__ == '__main__':
+    # TODO: Fix Bug
+    print("Pycharm is Awesome")
+    print("Hello")
+    name = "Ahmed"
+    print(name)
+    print(add_numbers(2, 2))
+    alex_method("Jean")
